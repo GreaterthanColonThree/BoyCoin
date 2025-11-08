396332277b6e00ddf43f77685bf433aa76e5ed25
FemboyCodeDev <charsarg@spifftech.uk>
2025-11-08 08:44:09 +0000

    Update chainparams.cpp
    Gave it some new magic bytes

--- src/kernel/chainparams.cpp
+++ src/kernel/chainparams.cpp
@@ -126,10 +126,10 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 32-bit integer with any alignment.
          */
-        pchMessageStart[0] = 0xf9;
-        pchMessageStart[1] = 0xbe;
-        pchMessageStart[2] = 0xb4;
-        pchMessageStart[3] = 0xd9;
+        pchMessageStart[0] = 0x42;
+        pchMessageStart[1] = 0x4f;
+        pchMessageStart[2] = 0x59;
+        pchMessageStart[3] = 0x53;
         nDefaultPort = 8333;
         nPruneAfterHeight = 100000;
         m_assumed_blockchain_size = 810;
